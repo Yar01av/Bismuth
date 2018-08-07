06c3f0cd1427f49b93055797e93db02476e2d915
Yar0s1av <yaroslavn99@gmail.com>
2018-08-07 19:29:32 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, url_for, render_template, request, jsonify, send_from_directory
+from datetime import datetime
+from werkzeug import secure_filename
+import os
+
+
+UPLOAD_FOLDER = "./images"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+base_path = os.path.abspath(os.path.dirname(__file__))
+
+def get_text_from_lines(preceeding_text, text_file_lines):
+	print(text_file_lines)
+	lines_with_extras = list(filter(lambda line: preceeding_text in line, text_file_lines))
+
+	return [text_line[len(preceeding_text) : ] for text_line in lines_with_extras]
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+print(__name__)
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port="5000", debug=True)
+
+@app.route('/')
+def show_all():
+	return render_template('home.html')
+
+@app.route('/new-post/', methods = ['POST', 'GET'])
+def new_post():
+	if request.method == "POST":
+		with open(os.path.join(base_path, "static", "post_log.txt"), "a") as log:
+			f = request.files["new-post-image"]
+
+			#Make a note of the post
+			log.write("New post made on %s. \n" % datetime.now().strftime("%Y-%m-%d %H:%M"))
+			log.write("Heading: " + request.form["new-post-title"] + "\n")
+			log.write("Content: " + request.form["new-post-text"].replace("\r\n", " ").replace("\n", " ") + "\n")
+			log.write("Image name: " + secure_filename(f.filename) + "\n")
+
+			#Save the image
+			f.save(app.config['UPLOAD_FOLDER'] + '/' + secure_filename(f.filename))
+
+			return "The post was successfully posted!"
+	else:
+		with open(os.path.join(base_path, "static", "post_log.txt"), "r") as log:
+			#Get the posts
+			text_lines = log.readlines()
+			heading_from_lines = get_text_from_lines("Heading: ", text_lines)
+			content_from_lines = get_text_from_lines("Content: ", text_lines)
+			imgs_from_lines = get_text_from_lines("Image name: ", text_lines)
+
+			return jsonify(dict(headings = heading_from_lines, contents = content_from_lines, img_name = imgs_from_lines))
+
+@app.route("/images/<path:image_name>", methods = ["GET"])
+def image_fetch(image_name):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], image_name)
---
+++ static/script.js
@@ -0,0 +1,88 @@
+function flush_new_post_ui() {
+	$("#post-form").remove();
+	$("#curtain").remove();
+	$(".post").remove();
+
+	updatePostList();
+}
+
+function addPostButtonEvent() {
+	$("body").append("<div id='curtain'></div>");
+	$("body").append(" \
+						<form id='post-form' method=POST enctype='multipart/form-data'> \
+							<div class='form-group'> \
+								<label>Title of the new post</label> \
+								<input id='title-field' class='form-control' name='new-post-title'></input> \
+								 \
+							</div> \
+							<div class='form-group'> \
+								<label>Text of the new post</label> \
+								<textarea class='form-control' name='new-post-text'></textarea> \
+								 \
+							</div> \
+							<div class='form-group'> \
+								<label>Choose an image to upload</label> \
+								<input id='upload-button' type=file class='form-control-file' name=new-post-image></input> \
+								 \
+							</div> \
+							<input type='submit' class='btn secondary-bg-clr' rows=3 value='Submit a new post'></input> \
+							<button type='button' class='btn secondary-bg-clr' onclick=flush_new_post_ui() rows=4>Cancel</button> \
+						</form> \
+					");
+
+	//Assocuated events
+	$("#post-form").submit(function(event){
+		//prevents refreshing
+		event.preventDefault();
+
+		//sends the data to the server
+		var formData = new FormData($("#post-form")[0]);
+
+		$.ajax({
+			url: postEndpoint,
+			processData: false,
+			method: "POST",
+			data: formData,
+			contentType: false,
+            processData: false,
+            async: false,
+			success: function(respData, status, jqXHR) {
+				console.log(respData);
+			}
+		});
+
+		//restores the normal page and updates it
+		flush_new_post_ui();
+	});
+	$("#curtain").click(flush_new_post_ui);
+	$("#title-field").keydown(function(event){
+	if (event.keyCode==13) {
+		event.preventDefault();
+		return false;
+	}
+});
+}
+
+//updates the list of posts
+function updatePostList() {
+	$.ajax({
+			url: postEndpoint,
+			method: "GET",
+			async: false,
+			success: function(respData, status, jqXHR) {
+				for(var i = 0; i < respData["contents"].length; i++) {
+					$("body").append('<div class="border media mb-2 shadow-sm post"> \
+	  									<img class="mr-3" src="./images/' + respData["img_name"][i] + '"> \
+	  									<div class="media-body"> \
+	    									<h5 class="mt-0">' + respData["headings"][i] + '</h5> \
+	    										' + respData["contents"][i] + ' \
+	  									</div>\
+									</div>')
+				}
+			}
+		})
+}
+
+
+//Start by updating the list of posts
+$(document).ready(updatePostList);
